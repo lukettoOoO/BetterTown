4e5fed57af67d63375470b320ee86cd9f8436c6f
lukettoOoO <110674888+lukettoOoO@users.noreply.github.com>
2025-04-03 23:13:25 +0300

    testing multiple panel layout

---
+++ Implementation/BetterTown/src/main/java/com/mycompany/bettertown/BetterTown.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bettertown;
+
+/**
+ *
+ * @author luca
+ */
+public class BetterTown {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
